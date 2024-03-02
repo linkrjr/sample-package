73938cd17047bf51073e14c3a78526d167b9aba3
Ronaldo Gomes <rgomes@tyro.com>
2024-03-02 20:03:54 +1100

    initial commit

--- Package.swift
+++ Package.swift
@@ -15,9 +15,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SamplePackage"),
+            name: "SamplePackage",
+            path: "SamplePackage"),
         .testTarget(
-            name: "SamplePackageTests",
-            dependencies: ["SamplePackage"]),
+            name: "SamplePackageTests",            
+            dependencies: ["SamplePackage"],
+            path: "SamplePackage"),
     ]
 )
