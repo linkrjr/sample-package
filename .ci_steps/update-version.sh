ee01b6d39991e7c68ab3ca012f1a68ba85d5c75e
Ronaldo Gomes <rgomes@tyro.com>
2024-03-05 16:09:55 +1100

    updates

--- .ci_steps/update-version.sh
+++ .ci_steps/update-version.sh
@@ -3,13 +3,12 @@ REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
 OLD_VERSION="$1"
 NEW_VERSION="$2"
 VB_VERSION=$(<VERSION)
-VERSION_FILE=./VERSION
 
-sed -i "s/\(version *= *['\"]*\)${VB_VERSION}\(['\"]*\)/\1${NEW_VERSION}\2/" ${VERSION_FILE}
+echo $NEW_VERSION > VERSION
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git add $VERSION_FILE
+git add VERSION
 git commit -m "Bump version from $OLD_VERSION to $NEW_VERSION [skip ci]"
 git push $REPO
 
