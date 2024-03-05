0d69ff93ae4122f0966dfc889e5929d8ce80dbba
Ronaldo Gomes <rgomes@tyro.com>
2024-03-05 22:14:59 +1100

    change to action

--- .ci_steps/update-version.sh
+++ .ci_steps/update-version.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
-REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
-OLD_VERSION="$1"
-NEW_VERSION="$2"
-VB_VERSION=$(<VERSION)
 
-echo $NEW_VERSION > VERSION
+NEW_VERSION="$1"
 
-# git config user.name "${GITHUB_ACTOR}"
-# git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-# git add VERSION
-# git commit -m "Bump version from $OLD_VERSION to $NEW_VERSION [skip ci]"
-# git push $REPO
-
-# check version is bumped
-CURRENT_VERSION=$(<VERSION)
-echo "CURRENT_VERSION=${CURRENT_VERSION}" >> $GITHUB_OUTPUT
+echo $1 > VERSION
