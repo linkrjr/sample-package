e6e03e0c34529bf176fb5959e51c42dcf99dcb3b
Ronaldo Gomes <rgomes@tyro.com>
2024-03-06 13:45:48 +1100

    updates

--- SamplePackageApp/SamplePackageApp/ContentView.swift
+++ SamplePackageApp/SamplePackageApp/ContentView.swift
@@ -11,7 +11,7 @@ import SamplePackage
 struct ContentView: View {
     var body: some View {
         VStack {
-					Text("Version: \(SamplePackage.version) asdf aasdf asdf asdf")
+					Text("Version: \(SamplePackage.version)")
         }
         .padding()
     }
